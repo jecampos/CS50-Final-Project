2a2f291894794882125d86641a36476a0386efbd
jecampos <66586356+jecampos@users.noreply.github.com>
2020-07-12 02:46:31 -0500

    stop feature

--- practice_record.py
+++ practice_record.py
@@ -2,10 +2,15 @@ import sounddevice as sd
 import numpy as np
 
 fs = 48000
-duration = 3  # seconds
+duration = 3600  # 1 hr limit
 
 myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=2, dtype='float64')
-sd.wait()
+
+while True:
+    command = input("say stop to finish recording: ")
+    if command == "stop":
+        sd.stop()
+        break
 
 sd.play(myrecording, fs)
 sd.wait()
