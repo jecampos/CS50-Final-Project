27fa76b75552cb97e423172a2828cddda64eaa1b
jecampos <66586356+jecampos@users.noreply.github.com>
2020-07-12 17:29:19 -0500

    First GUI Example of playback

--- practice_gui.py
+++ practice_gui.py
@@ -1,5 +1,39 @@
+import tkinter as tk
+import numpy as np
+import sounddevice as sd
+from datetime import datetime
+from PIL import Image, ImageTk
 
 
+# creating tkinter object
+root = tk.Tk()
 
+# variables for recording
+fs = 48000
+duration = 360 
+
+# start recording
+myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=2, dtype='float64')
+
+# stopRecording method thats plays back audio when clicked
+def stopRecording():
+    sd.stop()
+    statusLabel['text'] = "Playback"
+    sd.play(myrecording, fs)
+
+
+# status of recording label
+statusLabel = tk.Label(root, text="Start Recording")
+statusLabel.pack()
+
+# creating stop button
+stopPhoto = ImageTk.PhotoImage(Image.open("stop_button.png"))
+
+stopButton = tk.Button(root, image=stopPhoto, height=150, width=150, command= lambda: stopRecording())
+stopButton.pack()
+
+
+# run tkinter
+root.mainloop()
 
 # Image by OpenClipart-Vectors from Pixabay https://pixabay.com/vectors/sound-button-glossy-set-player-145674/
