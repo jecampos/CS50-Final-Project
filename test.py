52a1bcab1d39c673446787b6ce0c541335054f06
Ojou Nii <jiankarlo99@gmail.com>
2020-07-12 22:55:47 +0800

    Added test.py

--- test.py
+++ test.py
@@ -7,28 +7,31 @@ duration = 360  # 1 hr limit
 
 myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=2, dtype='float64')
 # start timer of recording
-start = datetime.now()
-end = 0
+print("[START RECORDING]")
+startRecording = datetime.now()
+endRecording = 0
 
 while True:
     command = input("say stop to finish recording: ")
     if command == "stop":
         # end timer of recording
-        end = datetime.now()
+        endRecording = datetime.now()
         sd.stop()
         break
 
 # get the record time by subtracting end with start
-recordTime = end - start
-print(recordTime)
+recordTime = endRecording - startRecording
+print(f"[RECORD TIME] {recordTime}...")
 
 # play recording
+print("[PLAYBACK]")
 sd.play(myrecording, fs)
 
 # start timer 
-playStart = datetime.now()
-playEnd = datetime.now()
+playRecord = datetime.now()
+endRecord = datetime.now()
 # don't end the program while recording is still playing
-while playEnd - playStart <= recordTime:
-    print(playEnd - playStart)
-    playEnd = datetime.now()
+while endRecord - playRecord <= recordTime:
+    endRecord = datetime.now()
+
+print("[END OF PLAYBACK]")
