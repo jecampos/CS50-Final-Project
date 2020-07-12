7ab3d45d6ec90c8bb4f0c1aa3c8bd563627d0a61
Ojou Nii <jiankarlo99@gmail.com>
2020-07-12 14:57:28 +0800

    pygame visualization

--- practice_sound.py
+++ practice_sound.py
@@ -1,12 +1,42 @@
 import matplotlib 
 import sounddevice as sd
 import numpy as np
+import pygame
 
-def print_sound(indata, outdata, frames, time, status):
+WIDTH, HEIGHT = 600, 400
+x = 0
+
+pygame.init()
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def print_sound(indata, frames, time, status):
+    global x
+
     volume_norm = np.linalg.norm(indata)*10
-    print ("|" * int(volume_norm))
+    # print (int(volume_norm))
+    pygame.draw.rect(screen, (255,255,255), (x, HEIGHT - int(volume_norm), 1, int(volume_norm)))
+    x += 1
 
-with sd.Stream(callback=print_sound):
-    sd.sleep(10000)
+    if x > WIDTH:
+        print("here")
+        screen.fill((0,0,0))
+        x = 0
+
+    pygame.display.update()
+
+
+while True:
+
+    # screen.fill((0, 0, 0))
+
+    with sd.InputStream(callback=print_sound):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+        sd.sleep(100000)
+
 
 #  From https://stackoverflow.com/questions/40138031/how-to-read-realtime-microphone-audio-volume-in-python-and-ffmpeg-or-similar
