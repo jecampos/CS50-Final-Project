26ebae384808d10c0be17168ffd2325c3a0371ca
Ojou Nii <jiankarlo99@gmail.com>
2020-07-14 03:22:39 +0800

    real time visualization using matplotlib

---
+++ audio.py
@@ -0,0 +1,41 @@
+import pyaudio 
+import struct
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# %matplotlib tk
+
+CHUNK = 1024  * 4
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+
+p = pyaudio.PyAudio()
+
+stream = p.open(
+	format=FORMAT,
+	channels=CHANNELS,
+	rate=RATE,
+	input=True,
+	output=True,
+	frames_per_buffer=(CHUNK)
+)
+
+plt.ion()
+fig, ax = plt.subplots()
+
+x = np.arange(0, CHUNK * 2, 2)
+line, = ax.plot(x, np.random.rand(CHUNK))
+ax.set_ylim(0, 255)
+ax.set_xlim(0, CHUNK)
+
+
+while True:
+	# print("heer")
+	data = stream.read(CHUNK)
+	data_int = np.array(struct.unpack(str(2 * CHUNK) + 'B', data), dtype='b')[::2] + 128
+	line.set_ydata(data_int)
+	fig.canvas.draw()
+	fig.canvas.flush_events()
+
