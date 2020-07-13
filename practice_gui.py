5e689beebb3edcf30a6e5759a6c86a29f588fa64
jecampos <66586356+jecampos@users.noreply.github.com>
2020-07-12 22:19:58 -0500

    better icons

--- practice_gui.py
+++ practice_gui.py
@@ -26,10 +26,10 @@ def stopRecording():
 statusLabel = tk.Label(root, text="Start Recording")
 statusLabel.pack()
 
-# creating stop button
-stopPhoto = ImageTk.PhotoImage(Image.open("resources\\50px_stop_button.png"))
+# creating stop button (recommended size 80x80 pixels as it looks pretty good)
+stopPhoto = ImageTk.PhotoImage(Image.open("resources\\stop-80.png"))
 
-stopButton = tk.Button(root, image=stopPhoto, height=50, width=50, command= lambda: stopRecording())
+stopButton = tk.Button(root, image=stopPhoto, height=80, width=80, command= lambda: stopRecording())
 stopButton["border"] = "0" # making button round
 stopButton.pack()
 
@@ -37,4 +37,5 @@ stopButton.pack()
 # run tkinter
 root.mainloop()
 
-# Image by OpenClipart-Vectors from Pixabay https://pixabay.com/vectors/sound-button-glossy-set-player-145674/
+# Image by OpenClipart-Vectors from Pixabay https://pixabay.com/vectors/sound-button-glossy-set-player-145674/\
+# also from https://icons8.com/icon/set/record%20icon/windows
