b4f32123229a94010c0483a692212f4abe597601
Ojou Nii <jiankarlo99@gmail.com>
2020-07-15 14:12:33 +0800

    bad changes

--- audio.py
+++ audio.py
@@ -3,11 +3,17 @@ import pyaudio
 import struct
 import numpy as np
 import matplotlib.pyplot as plt
-import time
+import tkinter as tk
 
 plt.style.use("ggplot")
 
-CHUNK = 1024 * 4
+# creating tkinter object
+# root = tk.Tk()
+# root.geometry("800x700")
+# canvas = tk.Canvas(width=600, height=400)
+# canvas.pack()
+
+CHUNK = 600
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
@@ -21,21 +27,32 @@ stream = p.open(
 	input=True,
 	output=True,
 	frames_per_buffer=CHUNK
- )
- 
+)
+
 plt.ion()
 fig, ax = plt.subplots()
 
-x = np.arange(0, CHUNK)
+x = np.arange(0, 600)
 data = stream.read(CHUNK)
 data_int16 = struct.unpack(str(CHUNK) + 'h', data)
-line, = ax.plot(x, data_int16)
-ax.set_ylim([-2**15,(2**15)-1])
+data_new = []
+for i in range(len(data_int16)):
+	data_new.append(data_int16[i] % 600)
+line, = ax.plot(x, data_new)
+ax.set_ylim(-600, 600)
 
 
 while True:
 	data = struct.unpack(str(CHUNK) + 'h', stream.read(CHUNK))
-	line.set_ydata(data)
+
+	data_new = []
+	for i in range(len(data_int16)):
+		if data[i] < 0:
+			data_new.append(-1 * (abs(data[i]) % 600))
+		else:
+			data_new.append(data[i] % 600)
+
+	line.set_ydata(data_new)
 	fig.canvas.draw()
 	fig.canvas.flush_events()
 
--- practice_gui.py
+++ practice_gui.py
@@ -2,18 +2,21 @@ import tkinter as tk
 import numpy as np
 import sounddevice as sd
 from datetime import datetime
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageDraw
 
 
 # creating tkinter object
 root = tk.Tk()
+root.geometry("600x600")
+canvas = tk.Canvas(width=600, height=300)
+canvas.pack()
 
 # variables for recording
 fs = 48000
 duration = 360 
 
 # start recording
-myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=2, dtype='float64')
+myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=1, dtype='int')
 
 # stopRecording method thats plays back audio when clicked
 def stopRecording():
@@ -33,8 +36,18 @@ stopButton = tk.Button(root, image=stopPhoto, height=80, width=80, command= lamb
 stopButton["border"] = "0" # making button round
 stopButton.pack()
 
+def draw():
+    x = 0
+    for i in range(len(myrecording) // 1000):
+        print(x)
+        canvas.create_rectangle(x, 300 - int(myrecording[i] % 300), x, 300, fill="gray")
+        canvas.pack()
+        x += 1
+        if x > 600:
+            x = 0
 
 # run tkinter
+root.after(0, draw)
 root.mainloop()
 
 # Image by OpenClipart-Vectors from Pixabay https://pixabay.com/vectors/sound-button-glossy-set-player-145674/\
