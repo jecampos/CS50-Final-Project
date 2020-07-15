1a1361efc842fdf6c14f0b351ec041ff6f83c452
jecampos <66586356+jecampos@users.noreply.github.com>
2020-07-15 01:07:20 -0500

    practice gui functionality but not working

--- practice_gui.py
+++ practice_gui.py
@@ -1,41 +1,70 @@
 import tkinter as tk
 import numpy as np
-import sounddevice as sd
+import pyaudio
 from datetime import datetime
 from PIL import Image, ImageTk
 
+CHUNK = 600
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
 
-# creating tkinter object
-root = tk.Tk()
+p = pyaudio.PyAudio()
 
-# variables for recording
-fs = 48000
-duration = 360 
+frames = [] # frames to store audio 
 
-# start recording
-myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=2, dtype='float64')
+def main():
+    # creating tkinter object & setting basic settings
+    root = tk.Tk()
+    root.geometry("800x700")
 
-# stopRecording method thats plays back audio when clicked
-def stopRecording():
-    sd.stop()
+    # status of recording 
+    statusLabel = tk.Label(root, text="Start Recording")
+    statusLabel.place(x=350, y=100)
+
+    # creating record button (recommended size 80x80 pixels icons as it looks pretty good)
+    recordPhoto = ImageTk.PhotoImage(Image.open("resources\\record-80.png"))
+
+    recordButton = tk.Button(root, image=recordPhoto, height=40, width=40, command = lambda: startRecording(statusLabel))
+    recordButton["border"] = "0"
+    recordButton.place(x=200, y=400)
+
+    # creating stop button
+    stopPhoto = ImageTk.PhotoImage(Image.open("resources\\stop-80.png"))
+
+    # keep size to 40
+    stopButton = tk.Button(root, image=stopPhoto, height=40, width=40, command= lambda: stopRecording(statusLabel))
+    stopButton["border"] = "0" # making button round
+    stopButton.place(x=275, y=400)
+
+    # run tkinter
+    root.mainloop()
+
+def startRecording(statusLabel):
+
+    statusLabel['text'] = "Recording..."
+     
+    stream = p.open(
+	format=FORMAT,
+	channels=CHANNELS,
+	rate=RATE,
+	input=True,
+	output=True,
+	frames_per_buffer=CHUNK
+
+    while (True):
+        data = stream.read(chunk)
+        frames.append(data)
+
+)
+
+
+def stopRecording(statusLabel):
     statusLabel['text'] = "Playback"
-    sd.play(myrecording, fs)
+
+main()
 
 
-# status of recording label
-statusLabel = tk.Label(root, text="Start Recording")
-statusLabel.pack()
-
-# creating stop button (recommended size 80x80 pixels as it looks pretty good)
-stopPhoto = ImageTk.PhotoImage(Image.open("resources\\stop-80.png"))
-
-stopButton = tk.Button(root, image=stopPhoto, height=80, width=80, command= lambda: stopRecording())
-stopButton["border"] = "0" # making button round
-stopButton.pack()
-
-
-# run tkinter
-root.mainloop()
 
 # Image by OpenClipart-Vectors from Pixabay https://pixabay.com/vectors/sound-button-glossy-set-player-145674/\
 # also from https://icons8.com/icon/set/record%20icon/windows
